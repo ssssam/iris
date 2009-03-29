7ce1f1339c02deaf5f9feb1da733089d51e50e07
Christian Hergert <chris@dronelabs.com>
2009-03-28 17:49:46 -0700

    Add test for verifying the number of deliveries through the port.

--- tests/port-1.c
+++ tests/port-1.c
@@ -4,6 +4,8 @@
 #include "mocks/mock-callback-receiver.h"
 #include "mocks/mock-callback-receiver.c"
 
+#define ITER_COUNT 1000000
+
 static void
 get_type1 (void)
 {
@@ -52,6 +54,34 @@ deliver1 (void)
 	g_assert (success == TRUE);
 }
 
+static void
+many_deliver1_cb (gpointer data)
+{
+	gint *counter = data;
+	g_atomic_int_inc (counter);
+}
+
+static void
+many_deliver1 (void)
+{
+	IrisPort     *port;
+	IrisReceiver *receiver;
+	IrisMessage  *msg;
+	gint          counter = 0;
+	gint          i;
+
+	port = iris_port_new ();
+	receiver = mock_callback_receiver_new (G_CALLBACK (many_deliver1_cb), &counter);
+	iris_port_set_receiver (port, receiver);
+
+	for (i = 0; i < ITER_COUNT; i++) {
+		msg = iris_message_new (1);
+		iris_port_post (port, msg);
+	}
+
+	g_assert_cmpint (counter, ==, ITER_COUNT);
+}
+
 gint
 main (int   argc,
       char *argv[])
@@ -63,6 +93,7 @@ main (int   argc,
 	g_test_add_func ("/port/get_type1", get_type1);
 	g_test_add_func ("/port/has_receiver1", has_receiver1);
 	g_test_add_func ("/port/deliver1", deliver1);
+	g_test_add_func ("/port/many_deliver1", many_deliver1);
 
 	return g_test_run ();
 }
