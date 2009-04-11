eae26e354830167f30f547f140f1b71aef887a5b
Christian Hergert <chris@dronelabs.com>
2009-04-11 04:19:57 -0700

    Add section header for iris-free-list docs.

--- iris/iris-free-list.c
+++ iris/iris-free-list.c
@@ -21,6 +21,22 @@
 #include "iris-free-list.h"
 
 /**
+ * SECTION:iris-free-list
+ * @short_description: A lock-free free-list data structure
+ *
+ * #IrisFreeList is a basic free-list used by algorithms wishing to
+ * control their own basic memory management. This can be very useful
+ * in a couple situations. It helps get around the problem of not having
+ * Garbage Collection to implement certain lock-free algorithms. However,
+ * in the process, it helps deal with allocator contention at the same
+ * time, but only after decent use.
+ *
+ * Technically, using a free-list is like leaking memory. So occasionally
+ * it will be a good idea to clean up the memory if it is relatively
+ * precious to your user. However, this feature is not yet supported.
+ */
+
+/**
  * iris_free_list_new:
  *
  * Creates a new instance of #IrisFreeList.
