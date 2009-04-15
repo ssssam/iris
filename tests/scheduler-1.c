461f244d6f6a01e922618d7b1d76cfd8554b6c29
Christian Hergert <chris@dronelabs.com>
2009-04-15 03:09:25 -0700

    Add thread count tests.

--- tests/scheduler-1.c
+++ tests/scheduler-1.c
@@ -7,6 +7,20 @@ test1 (void)
 	g_assert (IRIS_IS_SCHEDULER (iris_scheduler_default ()));
 }
 
+static void
+test2 (void)
+{
+	IrisScheduler* sched = iris_scheduler_default ();
+	g_assert_cmpint (iris_scheduler_get_min_threads (sched), ==, 1);
+}
+
+static void
+test3 (void)
+{
+	IrisScheduler* sched = iris_scheduler_default ();
+	g_assert_cmpint (iris_scheduler_get_max_threads (sched), >=, 2);
+}
+
 gint
 main (int   argc,
       char *argv[])
@@ -16,6 +30,8 @@ main (int   argc,
 	g_thread_init (NULL);
 
 	g_test_add_func ("/scheduler/default1", test1);
+	g_test_add_func ("/scheduler/get_min_threads1", test2);
+	g_test_add_func ("/scheduler/get_max_threads1", test3);
 
 	return g_test_run ();
 }
