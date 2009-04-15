76da7ec71081550591c7ff34cc29d53df9a4caa0
Christian Hergert <chris@dronelabs.com>
2009-04-15 11:17:41 -0700

    Add method for setting the error.

--- tests/task-1.c
+++ tests/task-1.c
@@ -25,6 +25,18 @@ test3 (void)
 	g_object_unref (task);
 }
 
+static void
+test4 (void)
+{
+	GError *error = g_error_new (1, 1, "Something %s", "blah");
+	IrisScheduler *sched = mock_scheduler_new ();
+	IrisTask *task = iris_task_new (NULL, NULL, NULL);
+	iris_task_set_scheduler (task, sched);
+	iris_task_take_error (task, error);
+	g_assert (iris_task_get_error (task) == error);
+	iris_task_take_error (task, NULL);
+}
+
 int
 main (int   argc,
       char *argv[])
@@ -36,6 +48,7 @@ main (int   argc,
 	g_test_add_func ("/task/new1", test1);
 	g_test_add_func ("/task/unref1", test2);
 	g_test_add_func ("/task/set_scheduler1", test3);
+	g_test_add_func ("/task/take_error1", test4);
 
 	return g_test_run ();
 }
