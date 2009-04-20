786ad23d61e288f0a9dbcd733d563160a9897aaa
Christian Hergert <chris@dronelabs.com>
2009-04-19 23:46:48 -0700

    Fix comparison logic.

--- iris/iris-coordination-arbiter.c
+++ iris/iris-coordination-arbiter.c
@@ -253,10 +253,10 @@ iris_coordination_arbiter_new (IrisReceiver *exclusive,
 	g_return_val_if_fail (concurrent == NULL || IRIS_IS_RECEIVER (concurrent), NULL);
 	g_return_val_if_fail (teardown   == NULL || IRIS_IS_RECEIVER (teardown),   NULL);
 
-	g_return_val_if_fail (exclusive  && exclusive  == concurrent, NULL);
-	g_return_val_if_fail (teardown   && exclusive  == teardown,   NULL);
-	g_return_val_if_fail (concurrent && concurrent == teardown,   NULL);
-	g_return_val_if_fail (exclusive || concurrent || teardown,    NULL);
+	g_return_val_if_fail (!exclusive  || exclusive  != concurrent, NULL);
+	g_return_val_if_fail (!teardown   || exclusive  != teardown,   NULL);
+	g_return_val_if_fail (!concurrent || concurrent != teardown,   NULL);
+	g_return_val_if_fail (exclusive   || concurrent || teardown,   NULL);
 	
 	arbiter = g_object_new (IRIS_TYPE_COORDINATION_ARBITER, NULL);
 	arbiter->priv->exclusive = exclusive ? g_object_ref (exclusive) : NULL;
