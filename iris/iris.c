21fd1d3fe370cb5ad1d3bb740c537f8ca1b9b1ab
Christian Hergert <chris@dronelabs.com>
2009-05-26 12:35:02 -0700

    Only allow initialization once.

--- iris/iris.c
+++ iris/iris.c
@@ -3,11 +3,20 @@
 #include "iris.h"
 #include "iris-debug.h"
 
+G_LOCK_DEFINE (initialized);
+static volatile gboolean initialized = FALSE;
+
 void
 iris_init (void)
 {
-	if (!g_thread_supported ())
-		g_thread_init (NULL);
-	g_type_init ();
-	iris_debug_init ();
+	G_LOCK (initialized);
+
+	if (!initialized) {
+		if (!g_thread_supported ())
+			g_thread_init (NULL);
+		g_type_init ();
+		iris_debug_init ();
+	}
+
+	G_UNLOCK (initialized);
 }
